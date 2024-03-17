573966fa7560697b58e0b40af589d23e4b91a53e
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-03-18 02:22:16 +0900

    designed stream pages

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 py-16 space-y-5">
       <div>
         <label className="w-full cursor-pointer text-gray-600 hover:text-orange-500 hover:border-orange-500 hover:transition-colors flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
           <svg
@@ -19,11 +19,26 @@ const Upload: NextPage = () => {
               strokeLinejoin="round"
             />
           </svg>
-
           <input className="hidden" type="file" />
         </label>
       </div>
-      <div className="my-5">
+      <div>
+        <label
+          className="mb-1 block text-sm font-medium text-gray-700"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <div className="rounded-md relative flex  items-center shadow-sm">
+          <input
+            id="name"
+            type="email"
+            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            required
+          />
+        </div>
+      </div>
+      <div>
         <label
           className="mb-1 block text-sm font-medium text-gray-700"
           htmlFor="price"
@@ -48,20 +63,20 @@ const Upload: NextPage = () => {
       <div>
         <label
           className="mb-1 block text-sm font-medium text-gray-700"
-          htmlFor="textarea"
+          htmlFor="description"
         >
           Description
         </label>
         <div>
           <textarea
-            id="textarea"
+            id="description"
             className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
             rows={4}
           />
         </div>
       </div>
       <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Upload product
+        Upload item
       </button>
     </div>
   );
