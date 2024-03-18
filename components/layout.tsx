22379937c28cff37e3a5b0307f611e2dc668b546
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-03-18 14:23:37 +0900

    add Layout & apply detail pages

---
+++ components/layout.tsx
@@ -0,0 +1,196 @@
+import { cls } from "@/libs/utils";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+//import { cls } from "../libs/utils";
+
+interface LayoutProps {
+  title?: string;
+  canGoBack?: boolean;
+  canGoHome?: boolean;
+  hasTabBar?: boolean;
+  undo?: boolean;
+  children: React.ReactNode;
+  addClassName?: string;
+}
+
+export default function Layout({
+  title,
+  canGoBack,
+  canGoHome,
+  undo,
+  hasTabBar,
+  children,
+}: LayoutProps) {
+  const { back } = useRouter();
+  const goBackHandler = () => back();
+
+  return (
+    <div>
+      <div
+        className={cls(
+          !canGoBack ? "justify-center" : "space-x-4 font-semibold",
+          "bg-white w-full max-w-xl text-lg font-medium px-5 py-3 text-gray-800 border-b fixed top-0 flex items-center"
+        )}
+      >
+        {undo && (
+          <button className="absolute left-4" onClick={goBackHandler}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        )}
+        {canGoBack ? (
+          <span className="space-x-2 flex">
+            <button onClick={goBackHandler}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 19.5 8.25 12l7.5-7.5"
+                />
+              </svg>
+            </button>
+            {canGoHome && (
+              <Link href="/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
+              </Link>
+            )}
+          </span>
+        ) : null}
+        {title ? <span>{title}</span> : null}
+      </div>
+      <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
+      {hasTabBar ? (
+        <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
+          <Link href="/">
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              <span>홈</span>
+            </div>
+          </Link>
+          <Link href="/community">
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z"
+                />
+              </svg>
+              <span>동네생활</span>
+            </div>
+          </Link>
+          <Link href="/streams">
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
+                />
+              </svg>
+              <span>라이브</span>
+            </div>
+          </Link>
+          <Link href="/chats">
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+                />
+              </svg>
+              <span>채팅</span>
+            </div>
+          </Link>
+          <Link href="profile">
+            <div className="flex flex-col items-center space-y-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+              <span>나의 당근</span>
+            </div>
+          </Link>
+        </nav>
+      ) : null}
+    </div>
+  );
+}
--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,24 +1,27 @@
+import Layout from "@/components/layout";
 import type { NextPage } from "next";
 
 const Chats: NextPage = () => {
   const fakeArr = Array.from({ length: 5 }, (_, k) => k + 1);
   return (
-    <div className="py-10 divide-y-[1px]">
-      {fakeArr.map((_, i) => (
-        <div
-          key={i}
-          className="flex px-4 py-3 cursor-pointer items-center space-x-3"
-        >
-          <div className="w-10 h-10 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">Steve Jebs</p>
-            <p className="text-sm text-gray-500">
-              See you tomorrow in the corner at 2pm!
-            </p>
+    <Layout title="채팅" hasTabBar>
+      <div className="py-10 divide-y-[1px]">
+        {fakeArr.map((_, i) => (
+          <div
+            key={i}
+            className="flex px-4 py-3 cursor-pointer items-center space-x-3"
+          >
+            <div className="w-10 h-10 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">Steve Jebs</p>
+              <p className="text-sm text-gray-500">
+                See you tomorrow in the corner at 2pm!
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
