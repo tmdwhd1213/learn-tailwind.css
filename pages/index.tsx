5593c3402710fbe90d922ac302926ed8559a1422
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-03-24 11:40:23 +0900

    conflict test

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ interface ProductsResponse {
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
   const { data } = useSWR<ProductsResponse>("/api/products");
+  console.log();
 
   return (
     <Layout title="홈" hasTabBar>
